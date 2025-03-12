9b274977c773e25f6d6158cb0ada50a2bf4dc603
Tisca Catalin <admin@tiscacatalin.com>
2025-03-12 14:47:21 +0200

    hero section for desktop

--- resources/views/components/welcome/hero.blade.php
+++ resources/views/components/welcome/hero.blade.php
@@ -1,6 +1,4 @@
-<div class="relative isolate overflow-hidden bg-gradient-to-b from-indigo-100/20">
-    {{-- <div class="absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:-mr-80 lg:-mr-96"
-        aria-hidden="true"></div> --}}
+<div class="relative isolate overflow-hidden">
     <div @class([
         "mx-auto max-w-7xl px-6 py-16 sm:py-24 lg:px-8",
         "grid grid-cols-1 lg:grid-cols-2 lg:gap-x-16 lg:items-center"
@@ -13,13 +11,17 @@
 
         <div class="space-y-8">
             <h1
-                class="max-w-2xl text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl lg:col-span-2 xl:col-auto">
-                We’re changing the way people connect</h1>
+                class="max-w-2xl text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl lg:col-span-2 xl:col-auto"
+            >
+                We’re changing the way people connect
+            </h1>
             <div class="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
-                <p class="text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">Anim aute id magna aliqua ad
+                <p class="text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
+                    Anim aute id magna aliqua ad
                     ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam
                     occaecat fugiat aliqua. Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
-                    cupidatat commodo.</p>
+                    cupidatat commodo.
+                </p>
                 <div class="mt-10 flex items-center gap-x-6">
                     <a 
                         href="{{ route("login")  }}"
@@ -33,5 +35,4 @@
             </div>
         </div>
     </div>
-    {{-- <div class="absolute inset-x-0 bottom-0 -z-10 h-24 bg-gradient-to-t from-white sm:h-32"></div> --}}
 </div>
