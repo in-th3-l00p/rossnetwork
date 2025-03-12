7f93e9564322b2b8f76c36cbb8324b5f7e27064e
Tisca Catalin <admin@tiscacatalin.com>
2025-03-12 13:15:24 +0200

    complete alpine animation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,6 +12,9 @@
 
         <!-- Styles -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <!-- Alpine.js -->
+        <script src="//unpkg.com/alpinejs" defer></script>
     </head>
     <body class="antialiased font-sans">
         <x-header />
