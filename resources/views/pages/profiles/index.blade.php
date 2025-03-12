b312db94e3c8c9c5019540632fd7ab7c100f1503
Tisca Catalin <admin@tiscacatalin.com>
2025-03-12 18:01:48 +0200

    added edit button for profiles

--- resources/views/pages/profiles/index.blade.php
+++ resources/views/pages/profiles/index.blade.php
@@ -34,6 +34,14 @@
         @foreach (auth()->user()->profiles as $profile)
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-6">
                 <div class="bg-light2 overflow-hidden shadow-sm sm:rounded-lg p-4 sm:p-8">
+                    <div class="flex justify-end items-center gap-2 mb-6">
+                        <a href="{{ route('profiles.edit', $profile->id) }}">
+                            <x-primary-button>
+                                <x-fas-pencil class="size-4 shrink-0 text-white" />
+                            </x-primary-button>
+                        </a>
+                    </div>
+
                     <p>{{ __("First name")  }}: {{ $profile->first_name }}</p>
                     <p>{{ __("Last name")  }}: {{ $profile->last_name }}</p>
                     <p>{{ __("Birth date")  }}: {{ $profile->birth_date }}</p>`
