697f75871c48eb7bdaec289bd4c1a3872f6739a4
Tisca Catalin <admin@tiscacatalin.com>
2025-03-12 13:12:05 +0200

    complete header & models created

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,10 +1,17 @@
+@php
+    $navigationLinks = [
+        ['name' => 'Profiles', 'route' => route('profiles.index', [], false)],
+        ['name' => 'Projects', 'route' => route('projects.index', [], false)],
+        ['name' => 'Events', 'route' => route('events.index', [], false)],
+    ];
+@endphp
 <header class="bg-white" x-data="{ isOpen: false }">
     <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
       <div class="flex flex-1">
         <div class="hidden lg:flex lg:gap-x-12">
-          <a href="#" class="text-sm/6 font-semibold text-gray-900">Product</a>
-          <a href="#" class="text-sm/6 font-semibold text-gray-900">Features</a>
-          <a href="#" class="text-sm/6 font-semibold text-gray-900">Company</a>
+          @foreach ($navigationLinks as $link)
+            <a href="{{ $link['route'] }}" class="text-sm/6 font-semibold text-gray-900">{{ $link['name'] }}</a>
+          @endforeach
         </div>
         <div class="flex lg:hidden">
           <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="isOpen = true">
@@ -20,7 +27,11 @@
         <img class="h-8 w-auto" src="/logo.svg" alt="logo">
       </a>
       <div class="flex flex-1 justify-end">
-        <a href="#" class="text-sm/6 font-semibold text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
+        @auth
+            <a href="{{ route('dashboard') }}" class="text-sm/6 font-semibold text-gray-900">Dashboard</a>
+        @else
+            <a href="{{ route('login') }}" class="text-sm/6 font-semibold text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
+        @endauth
       </div>
     </nav>
     <!-- Mobile menu, show/hide based on menu open state. -->
@@ -38,17 +49,24 @@
             </button>
           </div>
           <a href="#" class="-m-1.5 p-1.5">
-            <span class="sr-only">Your Company</span>
-            <img class="h-8 w-auto" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" alt="">
+            <span class="sr-only">RossNetwork</span>
+            <img class="h-8 w-auto" src="/logo.svg" alt="logo">
           </a>
           <div class="flex flex-1 justify-end">
-            <a href="#" class="text-sm/6 font-semibold text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
+            @auth
+                <a href="{{ route('dashboard') }}" class="text-sm/6 font-semibold text-gray-900">Dashboard</a>
+            @else
+                <a href="{{ route('login') }}" class="text-sm/6 font-semibold text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
+            @endauth
           </div>
         </div>
         <div class="mt-6 space-y-2">
-          <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Product</a>
-          <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Features</a>
-          <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Company</a>
+          @foreach ($navigationLinks as $link)
+            <a 
+                href="{{ $link['route'] }}" 
+                class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+            >{{ $link['name'] }}</a>
+          @endforeach
         </div>
       </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\EventController;
 
 Route::view('/', 'welcome');
 
@@ -11,5 +14,9 @@ Route::view('dashboard', 'dashboard')
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
+
+Route::resource("profiles", ProfileController::class);
+Route::resource("projects", ProjectController::class);
+Route::resource("events", EventController::class);
 
 require __DIR__.'/auth.php';
