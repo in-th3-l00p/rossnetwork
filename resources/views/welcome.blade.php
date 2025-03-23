752bc5bca61f4782e40a5ae493070f4c09a51bbe
intheloop <admin@tiscacatalin.com>
2025-03-23 09:28:15 +0200

    restored landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,27 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>RossNetwork</title>
+    <title>RossNetwork</title>
 
-        <!-- Styles -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
-        <!-- Alpine.js -->
-        <script src="//unpkg.com/alpinejs" defer></script>
-    </head>
-    <body class="antialiased font-sans">
-        <main class="w-screen h-screen flex flex-col">
-            <div class="flex-1 flex flex-col items-center justify-center">
-                <img src="/logo.svg" alt="logo" class="aspect-square w-32 mb-16">
-                <h1 class="text-dark text-xl sm:text-3xl md:text-6xl font-bold mb-4">
-                    RossNetwork
-                </h1>
-                <div class="grid grid-cols-[1fr_auto_1fr] gap-8 items-center justify-center">
-                    <h2 class="text-dark text-base sm:text-xl md:text-3xl font-bold self-end">
-                        Coming soon
-                    </h2>
-                    <div class="w-2 h-2 bg-dark rounded-full justify-self-center"></div>
-                    <a href="https://tiscacatalin.com/articles/announcing-ross-network">
-                        <h2 class="text-dark hover:text-darker transition-all text-base sm:text-xl md:text-3xl font-bold self-start">
-                            Learn more
-                        </h2>
-                    </a>
-                </div>
-            </div>
+    <!-- Styles -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-            <footer class="w-full text-center py-8">
-                <a 
-                    href="https://tiscacatalin.com" 
-                    class="text-dark hover:text-darker transition-all"
-                >
-                    All rights reserved © Tisca Catalin {{ date('Y') }}
-                </a>
-            </footer>
-        </main>
-    </body>
+    <!-- Alpine.js -->
+    <script src="//unpkg.com/alpinejs" defer></script>
+</head>
+<body class="antialiased font-sans">
+    <x-layout.header />
+    <x-welcome.hero />
+    <x-welcome.events />
+    <x-welcome.people />
+    <x-welcome.projects />
+    <x-layout.footer />
+</body>
 </html>
